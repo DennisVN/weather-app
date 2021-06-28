5315e57fbc4b0b48009349286ab175de8cc6c78b
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-28 18:25:16 +0200

    Dump var, let instead

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
+let button = document.querySelector('.button')
+let inputValue = document.querySelector('.inputValue')
 let name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+let desc = document.querySelector('.desc');
+let temp = document.querySelector('.temp');
 
 clickButton = () => {
     console.log(document.getElementById("run"))
@@ -10,9 +10,9 @@ clickButton = () => {
         fetch('http://api.openweathermap.org/data/2.5/weather?&units=metric&q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad')
         .then(response => response.json())
         .then (data => {
-        var nameValue = data ['name'];
-        var tempValue = data ['main']['temp'];
-        var descValue = data ['weather'][0]['description'];
+        let nameValue = data ['name'];
+        let tempValue = data ['main']['temp'];
+        let descValue = data ['weather'][0]['description'];
 
         name.innerHTML = nameValue;
         temp.innerHTML = tempValue;

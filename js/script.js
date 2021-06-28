71c1291f2d38cc0e23e17799494ab507734ab192
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-28 16:39:50 +0200

    Add gitignore, Fix button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-// Declare variables
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+clickButton = () => {
+    console.log(document.getElementById("run"))
+    document.getElementById("run").addEventListener("click", function() {
+    })
+};
+clickButton()
 
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad')
+/*fetch('http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad')
     .then(response => response.json())
-    .then (data => console.log(data))
+    .then (data => {
+        var nameValue = data['name'];
+        var tempValue = data ['main']['temp'];
+        var descValue = data ['weather'][0]['description'];
 
-.catch(err => alert("Wrong")) 
-/*WORKS : http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad*/
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+    })
+
+.catch(err => alert("Wrong")) */
+/* WORKS : http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad */

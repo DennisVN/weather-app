1f756719d40fb42283c164a0c504d2f4097516c6
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-28 18:24:00 +0200

    Base structure works, Degrees in Celcius

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+let name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
 clickButton = () => {
     console.log(document.getElementById("run"))
     document.getElementById("run").addEventListener("click", function() {
-    })
-};
-clickButton()
-
-
-/*fetch('http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad')
-    .then(response => response.json())
-    .then (data => {
-        var nameValue = data['name'];
+        fetch('http://api.openweathermap.org/data/2.5/weather?&units=metric&q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad')
+        .then(response => response.json())
+        .then (data => {
+        var nameValue = data ['name'];
         var tempValue = data ['main']['temp'];
         var descValue = data ['weather'][0]['description'];
 
@@ -18,5 +19,9 @@ clickButton()
         desc.innerHTML = descValue;
     })
 
-.catch(err => alert("Wrong")) */
+.catch(err => alert("Wrong City")) 
+    })
+};
+clickButton()
+
 /* WORKS : http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad */

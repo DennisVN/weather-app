fad25dbe38389e3b3a5f10643c4eda5e2cea47dc
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-28 13:08:00 +0200

    Start with HTML, Basic layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-let test = [1, 2, 3 ];
-console.log(test);
+// Create correct HTML field first. 
 
-
+/*(async () => {
+    let data = await fetch('https//api.openweathermap.org/data/2.5/forecast?q={antwerp}&appid={ba4146bd03d9855da2254b9e254e92ad}');
+    let main = await data.json();
+    console.log(main);
+})();  */

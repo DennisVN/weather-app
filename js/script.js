cb418e5056e116cbfe6b05a2bff543686fce7392
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-28 14:44:58 +0200

    Fetch for Antwerp as test.WIP fix button.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-// Create correct HTML field first. 
+// Declare variables
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
 
-/*(async () => {
-    let data = await fetch('https//api.openweathermap.org/data/2.5/forecast?q={antwerp}&appid={ba4146bd03d9855da2254b9e254e92ad}');
-    let main = await data.json();
-    console.log(main);
-})();  */
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad')
+    .then(response => response.json())
+    .then (data => console.log(data))
+
+.catch(err => alert("Wrong")) 
+/*WORKS : http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad*/

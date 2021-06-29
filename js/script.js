7b2e1f3af45b000e0f69412c43a935b1d124c7d2
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-29 10:22:35 +0200

    Done fix inputValue in link. Add picture and ready with basic setup :).

--- js/script.js
+++ js/script.js
@@ -7,21 +7,22 @@ let temp = document.querySelector('.temp');
 clickButton = () => {
     console.log(document.getElementById("run"))
     document.getElementById("run").addEventListener("click", function() {
-        fetch('http://api.openweathermap.org/data/2.5/weather?&units=metric&q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad')
+        fetch(`http://api.openweathermap.org/data/2.5/weather?&units=metric&q=${inputValue.value}&appid=ba4146bd03d9855da2254b9e254e92ad`)
         .then(response => response.json())
         .then (data => {
         let nameValue = data ['name'];
         let tempValue = data ['main']['temp'];
-        let descValue = data ['weather'][0]['description'];
+        let  descValue = data ['weather'][0]['description'];
 
         name.innerHTML = nameValue;
         temp.innerHTML = tempValue;
         desc.innerHTML = descValue;
     })
+   
 
 .catch(err => alert("Wrong City")) 
     })
 };
 clickButton()
 
-/* WORKS : http://api.openweathermap.org/data/2.5/weather?q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad */
+/* WORKS : http://api.openweathermap.org/data/2.5/weather?&units=metric&q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad */

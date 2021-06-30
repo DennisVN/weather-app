ae05a498b7378105b12ff8834392e433fd33ce4f
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-30 22:45:28 +0200

    ICONS WORK, WIP Layout and styling. Time for ice cream now .

--- js/script.js
+++ js/script.js
@@ -111,27 +111,31 @@ function displayResults (weather) {
     dayOneTemperatureDiv.innerHTML = `${Math.round(weather.list[8].main.temp)}<span> ° C</span>`;
     weatherDayOne.innerText = `${weather.list[8].weather[0].description}`;
     minMaxDayOne.innerText = `feels like ` + Math.round(weather.list[8].main.feels_like) + `°c`;
-    weatherIconDayOne.innerHTML = document.querySelector('.weather-icon-one').src=(`http://openweathermap.org/img/wn/${weather.list[8].weather[0].icon}@2x.png`)
+    weatherIconDayOne.innerHTML = document.querySelector('.weather-icon-one').src=(`http://openweathermap.org/img/wn/${weather.list[8].weather[0].icon}@2x.png`);
 
     dateDayTwo.innerText = `${weather.list[16].dt_txt}`;
     dayTwoTemperatureDiv.innerHTML = `${Math.round(weather.list[16].main.temp)}<span> ° C</span>`;
     weatherDayTwo.innerText = `${weather.list[16].weather[0].description}`;
     minMaxDayTwo.innerText = `feels like ` + Math.round(weather.list[16].main.feels_like) + `°c`;
+    weatherIconDayTwo.innerHTML = document.querySelector('.weather-icon-two').src=(`http://openweathermap.org/img/wn/${weather.list[16].weather[0].icon}@2x.png`);
 
     dateDayThree.innerText = `${weather.list[24].dt_txt}`;
     dayThreeTemperatureDiv.innerHTML = `${Math.round(weather.list[24].main.temp)}<span> ° C</span>`;
     weatherDayThree.innerText = `${weather.list[24].weather[0].description}`;
     minMaxDayThree.innerText = `feels like ` + Math.round(weather.list[24].main.feels_like) + `°c`;
+    weatherIconDayThree.innerHTML = document.querySelector('.weather-icon-three').src=(`http://openweathermap.org/img/wn/${weather.list[24].weather[0].icon}@2x.png`);
 
     dateDayFour.innerText = `${weather.list[32].dt_txt}`;
     dayFourTemperatureDiv.innerHTML = `${Math.round(weather.list[32].main.temp)}<span> ° C</span>`;
     weatherDayFour.innerText = `${weather.list[32].weather[0].description}`;
     minMaxDayFour.innerText = `feels like ` + Math.round(weather.list[32].main.feels_like) + `°c`;
+    weatherIconDayFour.innerHTML = document.querySelector('.weather-icon-four').src=(`http://openweathermap.org/img/wn/${weather.list[32].weather[0].icon}@2x.png`);
 
     dateDayFive.innerText = `${weather.list[39].dt_txt}`;
     dayFiveTemperatureDiv.innerHTML = `${Math.round(weather.list[39].main.temp)}<span> ° C</span>`;
     weatherDayFive.innerText = `${weather.list[39].weather[0].description}`;
     minMaxDayFive.innerText = `feels like ` + Math.round(weather.list[39].main.feels_like) + `°c`;
+    weatherIconDayFive.innerHTML = document.querySelector('.weather-icon-five').src=(`http://openweathermap.org/img/wn/${weather.list[39].weather[0].icon}@2x.png`);
 }
 
 

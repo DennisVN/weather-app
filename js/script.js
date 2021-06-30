e284741125f6a341cbbe0a64c29bf8cb8a2531f4
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-30 12:02:05 +0200

    Redo button, WIP assign cityDiv

--- js/script.js
+++ js/script.js
@@ -1,13 +1,53 @@
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
-let button = document.querySelector('.button')
-let inputValue = document.querySelector('.inputValue')
-let name = document.querySelector('.name');
-let desc = document.querySelector('.desc');
-let temp = document.querySelector('.temp');
+const cityDiv = document.querySelector('.location .city'); //Assigned
+const dateDiv = document.querySelector('.location .date');
+const currentTemperatureDiv = document.querySelector('.current .temperature');
+const descriptionDiv = document.querySelector('.current .weather');
+const averageDiv = document.querySelector('.min-max');
+const inputField = document.querySelector('.input-field');
+let current = new Date();
+const button = document.querySelector('.button')
+const inputValue = document.querySelector('.inputValue')
+
+
+clickButton = () => {
+    
+    document.getElementById("run").addEventListener("click", function() {
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?&units=metric&q=${inputValue.value}&appid=ba4146bd03d9855da2254b9e254e92ad`)
+        .then(response => response.json())
+        .then (data => {
+            let cityDiv = inputValue.value;
+
+            
+            console.log(inputValue.value);
+            console.log(cityDiv);
+            console.log(data.list);
+    })
+   
+
+        .catch(err => console.log(err)) 
+    })
+};
+clickButton()
+
+
 //let dayToday = document.querySelector('.day-today');
 
-// Update date and time
+
+
+
+
+
+
+
+
+
+/* WORKS : http://api.openweathermap.org/data/2.5/weather?&units=metric&q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad */
+
+// USE LATER 
+/*
+// Update date and time WORKING CLOCK
 setInterval(() => { //clock & date (put in README)
     const time = new Date();
     const month = time.getMonth();
@@ -19,30 +59,30 @@ setInterval(() => { //clock & date (put in README)
     const amPm = hour >=12 ? "PM" : "AM";
 
     timeEl.innerHTML = hoursin12HrFormat + ':' + minutes + '' + `<span id="am-pm">${amPm}</span>` 
-}, 1000)
+}, 1000)*/
+/* BUTTON WORKS, ADD LATER, STOP WASTING YOUR TIME BRUH -_-" 
 
 clickButton = () => {
-    
     document.getElementById("run").addEventListener("click", function() {
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?&units=metric&q=${inputValue.value}&appid=ba4146bd03d9855da2254b9e254e92ad`)
-        .then(response => response.json())
-        .then (data => {
-            nameValue = inputValue.value;
-
-            
-            console.log(inputValue.value);
-            console.log(nameValue);
-            console.log(data.list);
-    })
-   
-
-        .catch(err => console.log(err)) 
+        console.log("click")
     })
 };
-clickButton()
+clickButton()*/
+/* Add date function
+function setDate(d){
+    for (let i = 0; i < 5; i++) {
+        let months = [
+            'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'
+        ];
+        let weekdays = [
+            'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'
+        ];
+        let day = weekDays[d.getDay()];
+        let date = d.getDate();
+        let month = months [d.getMonth];
+        let year = d.getFullYear();
 
-
-/* WORKS : http://api.openweathermap.org/data/2.5/weather?&units=metric&q=Antwerp&appid=ba4146bd03d9855da2254b9e254e92ad */
-
-// FIXED LINK (DUMBASS) ... 
-// REDO HTML
+        return `${day} ${date} ${month} ${year}`
+    }
+}*/
